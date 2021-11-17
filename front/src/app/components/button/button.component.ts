354c72309455762138bdd3c7c0d96f97bfcd9f9d
Shubham Bane <44354399+shubhambane@users.noreply.github.com>
2021-11-17 11:53:38 +0530

    loginInForm_v1

---
+++ front/src/app/components/button/button.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  @Input()
+  text!: string;
+  @Input() color!: string;
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  onClick() {
+    alert("Logged In");
+  }
+
+}
---
+++ front/src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  email!: string;
+  password!: string;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onLogin() {
+    if (!this.email) {
+      alert('Login data missing!');
+    } else alert('User Logged!')
+  }
+}
